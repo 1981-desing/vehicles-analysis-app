fc8e08dfc48b4cb37ea71cf5e76b2d421dce1247
israel vitor cardoso <cardosoisraelvitorcardoso@gmail.com>
2026-08-13 12:36:41 -0300

    renomeia requirements.py para requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# vehicles-analysis-app
+# ANÁLISE DE VEÍCULOS
+
+É um aplicativo que fornece informações para quem precisa encontrar / trocar ou comprar um veículo usado, contém um histograma que mostra informações do odometro do veículo e um gráfico de scatter que compara o odometro com relação ao preço para facilitar a vida das pessoas que procuram por um veículo.
+
+## FUNCIONALIDADES
+
+- Um histograma que fornece informações do odometro de veículo.
+- Um gráfico para comparar o odometro com relação ao preço do veículo.
---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import plotly_express as px
+import streamlit as st 
+import nbformat as nbf
+
+veiculos_us = pd.read_csv('vehicles_us.csv')
+st.header('venda de veiculos')
+hist_button = st.button('mostrar histograma')
+if hist_button:
+    st.write("criando um histograma")
+    fig = px.histogram(veiculos_us, x='odometer')
+    st.plotly_chart(fig)
+
+st.write('analize de dados de veiculos')
+scatter_button = st.button('mostrar scatter plot')
+if scatter_button:
+    st.write("criando um scatter plot")
+    fig = px.scatter(veiculos_us, x='odometer', y='price')
+    st.plotly_chart(fig)
